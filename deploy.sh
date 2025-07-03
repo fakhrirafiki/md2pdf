d4bb86ef15eea8148c05e867682ee1309bc0ff56
Fakhri Rafiki <fakhri.rafiki@gmail.com>
2025-07-03 20:38:42 +0700

    fix/font

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,11 @@
 
 set -e  # Exit langsung jika ada error
 
-# Path script md2pdf lokal & global
 SCRIPT_NAME="md2pdf"
 LOCAL_PATH="./$SCRIPT_NAME"
 DEST_PATH="/usr/local/bin/$SCRIPT_NAME"
+THEMES_PATH="./themes"
+THEMES_DEST="/usr/local/bin/themes"
 
 if [ ! -f "$LOCAL_PATH" ]; then
   echo "❌ File $LOCAL_PATH tidak ditemukan!"
@@ -15,5 +16,13 @@ fi
 echo "🚀 Deploy $SCRIPT_NAME ke $DEST_PATH ..."
 sudo cp "$LOCAL_PATH" "$DEST_PATH"
 sudo chmod +x "$DEST_PATH"
+
+if [ -d "$THEMES_PATH" ]; then
+  echo "🚀 Deploy themes ke $THEMES_DEST ..."
+  sudo rm -rf "$THEMES_DEST" # Hapus theme lama, supaya selalu sinkron!
+  sudo cp -R "$THEMES_PATH" "$THEMES_DEST"
+else
+  echo "⚠️  Folder themes/ tidak ditemukan!"
+fi
 
 echo "✅ Deploy selesai! Sekarang kamu bisa pakai: $SCRIPT_NAME"
